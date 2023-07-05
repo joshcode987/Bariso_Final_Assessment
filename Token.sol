a2359e3c8f8e260bafa1d4b5da0fb3089997337b
joshcode987 <135768802+joshcode987@users.noreply.github.com>
2023-07-05 11:09:22 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -18,8 +18,8 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-    string public tokenName = "Meta";
-    string public tokenAbbrv = "MTA";
+    string public tokenName = "Josh";
+    string public tokenAbbrv = "Craft";
     uint public totalSupply = 0;
 
     // mapping variable here
@@ -38,4 +38,3 @@ contract MyToken {
         }
     }
 }
-
